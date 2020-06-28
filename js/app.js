16159638dd64ba131521f49ce5b3a81117eaf45d
corytveten <corytveten@gmail.com>
2020-06-28 09:34:14 -0700

    solution

--- js/app.js
+++ js/app.js
@@ -1,7 +1,37 @@
 const buttons = document.querySelectorAll('.btn');
+const imgDiv = document.querySelector('.img-container')
 
-function turnPage() {
-  console.log('turn');
-}
+counter = 0
+const cars = [
+  'contBcg-0.jpeg',
+  'contBcg-1.jpeg',
+  'contBcg-2.jpeg',
+  'contBcg-3.jpeg',
+  'contBcg-4.jpeg',
+]
 
- buttons.addEventListener("click", turnPage)
+let x = document.createElement('IMG');
+
+
+ buttons.forEach(function(button) {
+   button.addEventListener("click", function() {
+     if (button.classList.contains('btn-left')) {
+       console.log(counter);
+       imgDiv.style.background = `url("./img/${cars[counter]}")`;
+       counter --;
+       if(counter < 0){
+        counter = cars.length -1
+      }
+     } else {
+      console.log(counter);
+      imgDiv.style.background = `url("./img/${cars[counter]}")`;
+      //counter ++;
+      if (counter < cars.length - 1) {
+        counter ++;
+      } else {
+        counter = 0;
+      }
+     }
+   })
+ })
+
